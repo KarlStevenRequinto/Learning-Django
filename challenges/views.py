e4886377c1813400e84a8d87ce274f82c5592136
Karl Steven F. Requinto <lerdrobott@gmail.com>
2023-06-06 00:39:45 +0800

    dynamic views,returning html,path converts,reverse func

--- challenges/views.py
+++ challenges/views.py
@@ -1,39 +1,53 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 # Create your views here.
+
+monthly_challenges = {
+    "january": "january challenge",
+    "february": "february challenge",
+    "march": "march challenge",
+    "april": "april challenge",
+    "may": "may challenge",
+    "june": "june challenge",
+    "july": "july challenge",
+    "august": "august challenge",
+    "september": "september challenge",
+    "october": "october challenge",
+    "november": "november challenge",
+    "december": "december challenge",
+}
+
+
+def index(request):
+    list_items = ""
+    months = list(monthly_challenges.keys())
+
+    for month in months:
+        print(month)
+        capitalized_month = month.capitalize()
+        month_path = reverse("month-challenge", args=[month])
+        list_items += f"<li><a href=\"{month_path}\">{capitalized_month}</a></li>"
+
+    response_data = f"<ul>{list_items}</ul>"
+    return HttpResponse(response_data)
 
 
 def month_by_number(request, month):
-    return HttpResponse(month)
+    months = list(monthly_challenges.keys())
+
+    if month > len(months) or month == 0:
+        return HttpResponseNotFound("Nano ka ya?")
+
+    redirect_month = months[month-1]
+    redirect_path = reverse("month-challenge", args=[redirect_month])
+    return HttpResponseRedirect(redirect_path)
 
 
 def monthly_challenge(request, month):
-    text = None
-    if month.lower() == "january":
-        text = "january challenge"
-    elif month.lower() == "february":
-        text = "february challenge"
-    elif month.lower() == "march":
-        text = "march challenge"
-    elif month.lower() == "april":
-        text = "april challenge"
-    elif month.lower() == "may":
-        text = "may challenge"
-    elif month.lower() == "june":
-        text = "june challenge"
-    elif month.lower() == "july":
-        text = "july challenge"
-    elif month.lower() == "august":
-        text = "august challenge"
-    elif month.lower() == "september":
-        text = "september challenge"
-    elif month.lower() == "october":
-        text = "october challenge"
-    elif month.lower() == "november":
-        text = "november challenge"
-    elif month.lower() == "december":
-        text = "december challenge"
-    else:
-        text = "invalid month"
-        return HttpResponseNotFound(f"{text} nano ka")
-    return HttpResponse(f"{text} time mga paps!")
+    try:
+        text = monthly_challenges[month]
+        response_data = f"<h1>{text} time mga paps!</h1>"
+        return HttpResponse(response_data)
+    except:
+        return HttpResponseNotFound(f"<h1>{month} nano ka?<h1>")
